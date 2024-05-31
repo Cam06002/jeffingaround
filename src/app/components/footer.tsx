eb888822b281dc9de47c7dd4aeb86db6716dc5a1
JeffCEmploynet <jcampbell@employnet.com>
2024-05-31 13:58:00 -0700

    first commit

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Footer({}){
+  return(
+    <div>
+      <p>Hey I made this!</p>
+      <p>Here is my copyright</p>
+    </div>
+  )
+}
---
+++ src/app/components/header.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Header({}){
+  return(
+    <div>
+      <h1>This will eventually be a logo</h1>
+      <p>this will eventually be nav</p>
+    </div>
+  )
+}
